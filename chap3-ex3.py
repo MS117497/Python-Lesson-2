2bd7181f6fc5a411e19482cd115d479e5be69baf
Michael Spagnola <Michael@administrators-iMac.local>
2018-02-01 21:02:48 -0700

    Initial commit for chapter 3 exercise 3.

---
+++ chap3-ex3.py
@@ -0,0 +1,20 @@
+while True:
+        try:
+                score = float(raw_input("Enter score: "))
+		if 0 <= score <= 1.0:
+			break
+		else:
+			print "Bad score"
+        except ValueError:
+                print "Bad score"
+grade = 'F'
+if score >= 0.9:
+	grade = 'A'
+elif score >= 0.8:
+	grade = 'B'
+elif score >= 0.7:
+        grade = 'C'
+elif score >= 0.6:
+        grade = 'D'
+
+print grade
