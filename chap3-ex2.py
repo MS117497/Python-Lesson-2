3e26d1cb5c9f518fbcdb4aa2c3addb9c09d1200a
Michael Spagnola <Michael@administrators-iMac.local>
2018-02-01 20:32:23 -0700

    Initial commit for chapter 3 exercise 2.

---
+++ chap3-ex2.py
@@ -0,0 +1,18 @@
+while True:
+	try:
+		hours = float(raw_input("Enter Hours: "))
+		break
+	except ValueError:
+		print "Error, please use numeric input"
+while True:
+	try:
+		rate = float(raw_input("Enter Rate: "))
+                break
+        except ValueError:
+                print "Error, please use numeric input"
+othours = 0
+if hours > 40:
+	othours = hours - 40
+	hours = 40
+pay = othours*rate*1.5 + hours*rate
+print("Pay: " + str(pay))
